00093898976cdffdecbb4999e12ec61721a7257f
sergii-lagutin <sergey.v.lagutin@gmail.com>
2026-03-03 14:03:24 +0100

    Improve TriangleValidator.

--- src/main/scala/sl/challenge/TriangleValidator.scala
+++ src/main/scala/sl/challenge/TriangleValidator.scala
@@ -4,9 +4,11 @@ import sl.challenge.model.Types.Triangle
 
 object TriangleValidator {
   def isValid(triangle: Triangle): Boolean =
-    triangle
-      .zip(triangle.tail)
-      .forall { case (currentRow, nextRow) =>
-        currentRow.length == nextRow.length - 1
-      }
+    triangle.nonEmpty &&
+      triangle.head.length == 1 &&
+      triangle
+        .zip(triangle.tail)
+        .forall { case (currentRow, nextRow) =>
+          currentRow.length == nextRow.length - 1
+        }
 }
--- src/test/scala/sl/challenge/TriangleValidatorTest.scala
+++ src/test/scala/sl/challenge/TriangleValidatorTest.scala
@@ -8,7 +8,7 @@ class TriangleValidatorTest extends Specification {
       val triangle = Array(
         Array(1),
         Array(2, 3),
-        Array(4, 5, 6),
+        Array(4, 5, 6)
       )
 
       TriangleValidator.isValid(triangle) must beTrue
@@ -18,7 +18,21 @@ class TriangleValidatorTest extends Specification {
       val triangle = Array(
         Array(1),
         Array(2, 3),
-        Array(4, 5),
+        Array(4, 5)
+      )
+
+      TriangleValidator.isValid(triangle) must beFalse
+    }
+
+    "fail on empty triangle" >> {
+      TriangleValidator.isValid(Array()) must beFalse
+    }
+
+    "fail on truncated triangle" >> {
+      val triangle = Array(
+        Array(2, 3),
+        Array(4, 5, 6),
+        Array(7, 8, 9, 10)
       )
 
       TriangleValidator.isValid(triangle) must beFalse
