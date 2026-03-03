0420d4d819e8f37303512f657ded9f59c7254f81
sergii-lagutin <sergey.v.lagutin@gmail.com>
2026-03-03 12:09:08 +0100

    Add TriangleStdInReader.

--- src/main/scala/sl/challenge/TriangleReader.scala
+++ src/main/scala/sl/challenge/TriangleReader.scala
@@ -4,27 +4,44 @@ import cats.effect.{Resource, Sync}
 import cats.implicits.toFunctorOps
 import sl.challenge.model.Types.Triangle
 
-import scala.io.Source
+import scala.annotation.tailrec
+import scala.io.{Source, StdIn}
 import scala.util.Try
 
 trait TriangleReader[F[_]] {
   def read(): Resource[F, SourceReader[F]]
 }
 
-class SourceReader[F[_]: Sync](input: Source) {
+class SourceReader[F[_] : Sync](input: Source) {
   def getTriangle: F[Option[Triangle]] =
     Sync[F].delay(input.getLines()).map(lines => Try(lines.toArray.map(line => line.split(" ").map(_.toInt))).toOption)
 }
 
-class TriangleStringReader[F[_]: Sync](str: String) extends TriangleReader[F] {
+class TriangleStringReader[F[_] : Sync](str: String) extends TriangleReader[F] {
 
   override def read(): Resource[F, SourceReader[F]] =
     Resource.pure(Source.fromString(str)).map(new SourceReader[F](_))
 }
 
-class TriangleFileReader[F[_]: Sync](filename: String) extends TriangleReader[F] {
+class TriangleFileReader[F[_] : Sync](filename: String) extends TriangleReader[F] {
   override def read(): Resource[F, SourceReader[F]] =
     Resource
       .make(Sync[F].delay(Source.fromFile(filename)))(file => Sync[F].delay(file.close()))
       .map(new SourceReader(_))
+}
+
+class TriangleStdInReader[F[_] : Sync] extends TriangleReader[F] {
+
+  override def read(): Resource[F, SourceReader[F]] = {
+    @tailrec
+    def readLines(acc: List[String]): List[String] =
+      Option(StdIn.readLine()) match {
+        case Some(value) if value.nonEmpty => readLines(value :: acc)
+        case _ => acc
+      }
+
+    Resource.eval(Sync[F].delay(readLines(Nil).reverse.mkString("\n")))
+      .map(Source.fromString)
+      .map(new SourceReader[F](_))
+  }
 }
